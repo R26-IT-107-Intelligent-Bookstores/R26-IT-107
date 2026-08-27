21c8b32417e196cc65134e67b8c710b3243d352f
Saku1215 <sakusewwandi09@gmail.com>
2026-08-27 15:34:07 +0530

    feat(frontend-web): FedBook nav uses SSO handoff with logged-in username

--- frontend-web/app/view/[id]/page.tsx
+++ frontend-web/app/view/[id]/page.tsx
@@ -61,7 +61,14 @@ export default function BookDetailsPage() {
           <Link href="#" className="hover:text-teal-700 transition-colors pb-0.5">Community</Link>
           <Link href="/trendstock" className="hover:text-teal-700 transition-colors pb-0.5">Trendstock</Link>
           <Link href="http://172.104.167.123:8765/" className="hover:text-teal-700 transition-colors pb-0.5">EmoBooks</Link>
-          <Link href="http://169-58-243-99.nip.io/login" className="hover:text-teal-700 transition-colors pb-0.5">FedBook</Link>
+          <Link
+            href={loggedUser
+              ? `http://169-58-243-99.nip.io/sso?username=${encodeURIComponent(loggedUser)}`
+              : "http://169-58-243-99.nip.io/books"}
+            className="hover:text-teal-700 transition-colors pb-0.5"
+          >
+            FedBook
+          </Link>
         </div>
 
         <div className="flex items-center gap-4">
